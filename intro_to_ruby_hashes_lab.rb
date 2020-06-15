51ff1adbfc43123c05ea07abad47933a88d46881
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-15 02:18:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ end
 def monopoly_with_third_tier
   base_bash = {
     :railroads => {
-      :pieces => 4,
+      :pieces => "4",
       :rent_in_dollars => {
         :one_piece_owned => "25",
         :two_pieces_owned => "50",
